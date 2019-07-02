ed168d589608ec87a5b0353dfb9ee17e5af4c6f0
Evgeniy <support_tech@bk.ru>
2019-07-02 11:19:54 +0300

    Little Fix #3

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -3,9 +3,9 @@ package ru.skillbranch.devintensive.utils
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
         return if (fullName.isNullOrBlank()) {
-            "null" to "null"
+            null to null
         } else {
-            val parts: List<String>? = fullName.split(" ")
+            val parts: List<String>? = fullName.trim().split(" ")
 
             val firstName = parts?.getOrNull(0)
             val lastName = parts?.getOrNull(1)
@@ -16,10 +16,39 @@ object Utils {
     fun transliteration(payload: String, divider: String = " "): String {
         var word = ""
         val translationMap = mapOf(
-            'а' to "a",'b' to "б",'в' to "v",'г' to "g",'д' to "d",'е' to "e",'ё' to "e",'ж' to "zh",'з' to "z",
-            'и' to "i",'й' to "i",'к' to "k",'л' to "l",'м' to "m",'н' to "n",'о' to "o",'п' to "p",'р' to "r",
-            'с' to "s",'т' to "t",'у' to "u",'ф' to "f",'х' to "h",'ц' to "c",'ч' to "ch",'ш' to "sh",'щ' to "sh",
-            'ъ' to "",'ы' to "i",'ь' to "",'э' to "e",'ю' to "yu",'я' to "ya"
+            'а' to "a",
+            'b' to "б",
+            'в' to "v",
+            'г' to "g",
+            'д' to "d",
+            'е' to "e",
+            'ё' to "e",
+            'ж' to "zh",
+            'з' to "z",
+            'и' to "i",
+            'й' to "i",
+            'к' to "k",
+            'л' to "l",
+            'м' to "m",
+            'н' to "n",
+            'о' to "o",
+            'п' to "p",
+            'р' to "r",
+            'с' to "s",
+            'т' to "t",
+            'у' to "u",
+            'ф' to "f",
+            'х' to "h",
+            'ц' to "c",
+            'ч' to "ch",
+            'ш' to "sh",
+            'щ' to "sh",
+            'ъ' to "",
+            'ы' to "i",
+            'ь' to "",
+            'э' to "e",
+            'ю' to "yu",
+            'я' to "ya"
         )
 
         for (x in 0 until payload.length) {
@@ -41,16 +70,12 @@ object Utils {
     fun toInitials(firstName: String?, lastName: String?): String? {
         val name = firstName?.trim()
         val surname = lastName?.trim()
-        return if (!name.isNullOrEmpty() && !surname.isNullOrEmpty()) {
-            name[0].toUpperCase().toString() + surname[0].toUpperCase().toString()
-        } else if (!name.isNullOrEmpty() && surname.isNullOrEmpty()) {
-            name[0].toUpperCase().toString()
-        } else if (name.isNullOrEmpty() && !surname.isNullOrEmpty()) {
-            surname[0].toUpperCase().toString()
-        } else if (name == "" || surname == "") {
-            "null"
-        } else {
-            "null"
+        return when {
+            !name.isNullOrEmpty() && !surname.isNullOrEmpty() -> name[0].toUpperCase().toString() + surname[0].toUpperCase().toString()
+            !name.isNullOrEmpty() && surname.isNullOrEmpty() -> name[0].toUpperCase().toString()
+            name.isNullOrEmpty() && !surname.isNullOrEmpty() -> surname[0].toUpperCase().toString()
+            name == "" || surname == "" -> null
+            else -> null
         }
     }
 }
